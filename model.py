f2f63a4527e8b07b840ec94539a54e550dec62cf
Reid Williams <rwilliams@ideo.com>
2017-10-17 17:47:14 -0700

    added zspace sample layer to model

--- model.py
+++ model.py
@@ -11,31 +11,25 @@ Adapted from https://github.com/commaai/research/tree/master/models
 import os
 os.environ["KERAS_BACKEND"] = "tensorflow"
 import numpy as np
-import cv2 
-import tensorflow as tf
-# from utils import load, save
-from layers import Deconv2D
+# from layers import Deconv2D
 from keras import backend as K
-from keras.layers import Input, Conv2D, ELU, Flatten, BatchNormalization as BN
+from keras.layers import Input, Conv2D, ELU, Flatten, Dense, Lambda, BatchNormalization as BN
 # from keras.layers import Input, Dense, Reshape, Activation, Conv2D, LeakyReLU, Flatten, BatchNormalization as BN
-from keras.models import Sequential, Model
-from keras import initializations
-from functools import partial
-learning_rate = .0002
-beta1 = .5
-z_dim = 512
-normal = partial(initializations.normal, scale=.02)
+from keras.models import Model
 
-Class Vaegan():
-  def __init__(img_shape=(64, 64, 3), zlen=128):
+class Vaegan():
+  def __init__(self, img_shape=(64, 64, 3), zsize=128, batch_size=32):
     # Input image shape: x, y, channels
     self.img_shape = img_shape
     # latent (z) vector length
-    self.zlen = zlen
+    self.zsize = zsize
+    self.batch_size = batch_size
+    self._build_model()
 
-  def _build_encoder():
+  def _build_encoder(self):
     inputs = Input(shape=self.img_shape)
     
+    # Base number of 2D convolution filters. 64 is from paper.
     filters = 64
     t = inputs
     # Using Keras functional api
@@ -54,253 +48,63 @@ Class Vaegan():
     t = BN(axis=-1)(t)
     t = ELU(alpha=1)(t)
 
-    t = Flatten(t)
+    t = Flatten()(t)
     
     # In a variational autoencoder, the encoder outputs a mean and sigma vector
     # from which samples are drawn. In practice, treat the second output as
     # log(sigma**2), but we'll call it logsigma
-    mean = Dense(self.zlen, activation="elu")(t)
-    logsigma = Dense(self.zlen, activation="elu")(t)
+    mean = Dense(self.zsize, activation="elu")(t)
+    logsigma = Dense(self.zsize, activation="elu")(t)
 
     model = Model(inputs=inputs, outputs=[mean, logsigma])
     return model
 
-  def _build_decoder():
-    pass
-    HERE: build the decoder network.
-    PLAN: build decoder, implement custom loss function using keras backend, use
-    MSE for loss of X vs X' (for now). Test with some images.
+  def _sample(self, x):
+    """
+    Given a mean and logsigma value from the encoder, draw a zsize
+    vector from gassian and return. This is a defining feature of a variational
+    autoencoder.
+    """
 
-  def decode():
-    pass
+    mean, logsigma = x
+    sigma = K.exp(logsigma/2.0) # see Hands on machine learning, Geron, p. 435
 
+    # need to explicitly account for fact that input tensors include a whole
+    # batch. 
+    sample = K.random_normal_variable((self.batch_size, self.zsize), 0., 1.)
+    return sigma*sample + mean
 
+  def _sampling_layer(self):
+    """
+    Custom Keras layer that samples gaussian according to mean and sigma
+    from the encoder.
+    """
+    return Lambda(self._sample)
 
+    
+    # HERE: build the decoder network.
+    # PLAN: build decoder, implement custom loss function using keras backend, use
+    # MSE for loss of X vs X' (for now). Test with some images.
 
+  # def decode():
+  #   pass
 
+  def _build_model(self):
+    
+    encoder = self._build_encoder()
+    sampling_layer = self._sampling_layer()
+    inputs = Input(shape=self.img_shape)
 
-def generator(batch_size, gf_dim, ch, rows, cols):
-
-    model = Sequential()
-
-    model.add(Dense(gf_dim*8*rows[0]*cols[0], batch_input_shape=(batch_size, z_dim), name="g_h0_lin", init=normal))
-    model.add(Reshape((rows[0], cols[0], gf_dim*8)))
-    model.add(BN(mode=2, axis=3, name="g_bn0", gamma_init=mean_normal, epsilon=1e-5))
-    model.add(Activation("relu"))
-
-    model.add(Deconv2D(gf_dim*4, 5, 5, subsample=(2, 2), name="g_h1", init=normal))
-    model.add(BN(mode=2, axis=3, name="g_bn1", gamma_init=mean_normal, epsilon=1e-5))
-    model.add(Activation("relu"))
-
-    model.add(Deconv2D(gf_dim*2, 5, 5, subsample=(2, 2), name="g_h2", init=normal))
-    model.add(BN(mode=2, axis=3, name="g_bn2", gamma_init=mean_normal, epsilon=1e-5))
-    model.add(Activation("relu"))
-
-    model.add(Deconv2D(gf_dim, 5, 5, subsample=(2, 2), name="g_h3", init=normal))
-    model.add(BN(mode=2, axis=3, name="g_bn3", gamma_init=mean_normal, epsilon=1e-5))
-    model.add(Activation("relu"))
-
-    model.add(Deconv2D(ch, 5, 5, subsample=(2, 2), name="g_h4", init=normal))
-    model.add(Activation("tanh"))
-
-    return model
-
-
-def encoder(batch_size, df_dim, ch, rows, cols):
-
-    model = Sequential()
-    X = Input(batch_shape=(batch_size, rows[-1], cols[-1], ch))
-    model = Convolution2D(df_dim, 5, 5, subsample=(2, 2), border_mode="same",
-                          name="e_h0_conv", dim_ordering="tf", init=normal)(X)
-    model = LeakyReLU(.2)(model)
-
-    model = Convolution2D(df_dim*2, 5, 5, subsample=(2, 2), border_mode="same",
-                          name="e_h1_conv", dim_ordering="tf")(model)
-    model = BN(mode=2, axis=3, name="e_bn1", gamma_init=mean_normal, epsilon=1e-5)(model)
-    model = LeakyReLU(.2)(model)
-
-    model = Convolution2D(df_dim*4, 5, 5, subsample=(2, 2), name="e_h2_conv", border_mode="same",
-                          dim_ordering="tf", init=normal)(model)
-    model = BN(mode=2, axis=3, name="e_bn2", gamma_init=mean_normal, epsilon=1e-5)(model)
-    model = LeakyReLU(.2)(model)
-
-    model = Convolution2D(df_dim*8, 5, 5, subsample=(2, 2), border_mode="same",
-                          name="e_h3_conv", dim_ordering="tf", init=normal)(model)
-    model = BN(mode=2, axis=3, name="e_bn3", gamma_init=mean_normal, epsilon=1e-5)(model)
-    model = LeakyReLU(.2)(model)
-    model = Flatten()(model)
-
-    mean = Dense(z_dim, name="e_h3_lin", init=normal)(model)
-    logsigma = Dense(z_dim, name="e_h4_lin", activation="tanh", init=normal)(model)
-    meansigma = Model([X], [mean, logsigma])
-    return meansigma
+    tensor = inputs
+    tensor = encoder(tensor)
+    outputs = sampling_layer(tensor)
+    
+    self.model = Model(inputs=inputs, outputs=outputs)
 
 
-def discriminator(batch_size, df_dim, ch, rows, cols):
-    X = Input(batch_shape=(batch_size, rows[-1], cols[-1], ch))
-    model = Convolution2D(df_dim, 5, 5, subsample=(2, 2), border_mode="same",
-                          batch_input_shape=(batch_size, rows[-1], cols[-1], ch),
-                          name="d_h0_conv", dim_ordering="tf", init=normal)(X)
-    model = LeakyReLU(.2)(model)
-
-    model = Convolution2D(df_dim*2, 5, 5, subsample=(2, 2), border_mode="same",
-                          name="d_h1_conv", dim_ordering="tf", init=normal)(model)
-    model = BN(mode=2, axis=3, name="d_bn1", gamma_init=mean_normal, epsilon=1e-5)(model)
-    model = LeakyReLU(.2)(model)
-
-    model = Convolution2D(df_dim*4, 5, 5, subsample=(2, 2), border_mode="same",
-                          name="d_h2_conv", dim_ordering="tf", init=normal)(model)
-    model = BN(mode=2, axis=3, name="d_bn2", gamma_init=mean_normal, epsilon=1e-5)(model)
-    model = LeakyReLU(.2)(model)
-
-    model = Convolution2D(df_dim*8, 5, 5, subsample=(2, 2), border_mode="same",
-                          name="d_h3_conv", dim_ordering="tf", init=normal)(model)
-
-    dec = BN(mode=2, axis=3, name="d_bn3", gamma_init=mean_normal, epsilon=1e-5)(model)
-    dec = LeakyReLU(.2)(dec)
-    dec = Flatten()(dec)
-    dec = Dense(1, name="d_h3_lin", init=normal)(dec)
-
-    output = Model([X], [dec, model])
-
-    return output
 
 
-def get_model(sess, image_shape=(80, 160, 3), gf_dim=64, df_dim=64, batch_size=64,
-              name="autoencoder", gpu=0):
-    K.set_session(sess)
-    checkpoint_dir = './outputs/results_' + name
-    with tf.variable_scope(name), tf.device("/gpu:{}".format(gpu)):
-      # sizes
-      ch = image_shape[2]
-      rows = [image_shape[0]/i for i in [16, 8, 4, 2, 1]]
-      cols = [image_shape[1]/i for i in [16, 8, 4, 2, 1]]
 
-      # nets
-      G = generator(batch_size, gf_dim, ch, rows, cols)
-      G.compile("sgd", "mse")
-      g_vars = G.trainable_weights
-      print "G.shape: ", G.output_shape
 
-      E = encoder(batch_size, df_dim, ch, rows, cols)
-      E.compile("sgd", "mse")
-      e_vars = E.trainable_weights
-      print "E.shape: ", E.output_shape
 
-      D = discriminator(batch_size, df_dim, ch, rows, cols)
-      D.compile("sgd", "mse")
-      d_vars = D.trainable_weights
-      print "D.shape: ", D.output_shape
 
-      Z2 = Input(batch_shape=(batch_size, z_dim), name='more_noise')
-      Z = G.input
-      Img = D.input
-      G_train = G(Z)
-      E_mean, E_logsigma = E(Img)
-      G_dec = G(E_mean + Z2 * E_logsigma)
-      D_fake, F_fake = D(G_train)
-      D_dec_fake, F_dec_fake = D(G_dec)
-      D_legit, F_legit = D(Img)
-
-      # costs
-      recon_vs_gan = 1e-6
-      like_loss = tf.reduce_mean(tf.square(F_legit - F_dec_fake)) / 2.
-      kl_loss = tf.reduce_mean(-E_logsigma + .5 * (-1 + tf.exp(2. * E_logsigma) + tf.square(E_mean)))
-
-      d_loss_legit = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_legit, tf.ones_like(D_legit)))
-      d_loss_fake1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_fake, tf.zeros_like(D_fake)))
-      d_loss_fake2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_dec_fake, tf.zeros_like(D_dec_fake)))
-      d_loss_fake = d_loss_fake1 + d_loss_fake2
-      d_loss = d_loss_legit + d_loss_fake
-
-      g_loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_fake, tf.ones_like(D_fake)))
-      g_loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_dec_fake, tf.ones_like(D_dec_fake)))
-      g_loss = g_loss1 + g_loss2 + recon_vs_gan * like_loss
-      e_loss = kl_loss + like_loss
-
-      # optimizers
-      print "Generator variables:"
-      for v in g_vars:
-        print v.name
-      print "Discriminator variables:"
-      for v in d_vars:
-        print v.name
-      print "Encoder variables:"
-      for v in e_vars:
-        print v.name
-
-      e_optim = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(e_loss, var_list=e_vars)
-      d_optim = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(d_loss, var_list=d_vars)
-      g_optim = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(g_loss, var_list=g_vars)
-      tf.initialize_all_variables().run()
-
-    # summaries
-    sum_d_loss_legit = tf.scalar_summary("d_loss_legit", d_loss_legit)
-    sum_d_loss_fake = tf.scalar_summary("d_loss_fake", d_loss_fake)
-    sum_d_loss = tf.scalar_summary("d_loss", d_loss)
-    sum_g_loss = tf.scalar_summary("g_loss", g_loss)
-    sum_e_loss = tf.scalar_summary("e_loss", e_loss)
-    sum_e_mean = tf.histogram_summary("e_mean", E_mean)
-    sum_e_sigma = tf.histogram_summary("e_sigma", tf.exp(E_logsigma))
-    sum_Z = tf.histogram_summary("Z", Z)
-    sum_gen = tf.image_summary("G", G_train)
-    sum_dec = tf.image_summary("E", G_dec)
-
-    # saver
-    saver = tf.train.Saver()
-    g_sum = tf.merge_summary([sum_Z, sum_gen, sum_d_loss_fake, sum_g_loss])
-    e_sum = tf.merge_summary([sum_dec, sum_e_loss, sum_e_mean, sum_e_sigma])
-    d_sum = tf.merge_summary([sum_d_loss_legit, sum_d_loss])
-    writer = tf.train.SummaryWriter("/tmp/logs/"+name, sess.graph)
-
-    # functions
-    def train_d(images, z, counter, sess=sess):
-      z2 = np.random.normal(0., 1., z.shape)
-      outputs = [d_loss, d_loss_fake, d_loss_legit, d_sum, d_optim]
-      with tf.control_dependencies(outputs):
-        updates = [tf.assign(p, new_p) for (p, new_p) in D.updates]
-      outs = sess.run(outputs + updates, feed_dict={Img: images, Z: z, Z2: z2, K.learning_phase(): 1})
-      dl, dlf, dll, sums = outs[:4]
-      writer.add_summary(sums, counter)
-      return dl, dlf, dll
-
-    def train_g(images, z, counter, sess=sess):
-      # generator
-      z2 = np.random.normal(0., 1., z.shape)
-      outputs = [g_loss, G_train, g_sum, g_optim]
-      with tf.control_dependencies(outputs):
-        updates = [tf.assign(p, new_p) for (p, new_p) in G.updates]
-      outs = sess.run(outputs + updates, feed_dict={Img: images, Z: z, Z2: z2, K.learning_phase(): 1})
-      gl, samples, sums = outs[:3]
-      writer.add_summary(sums, counter)
-      # encoder
-      outputs = [e_loss, G_dec, e_sum, e_optim]
-      with tf.control_dependencies(outputs):
-        updates = [tf.assign(p, new_p) for (p, new_p) in E.updates]
-      outs = sess.run(outputs + updates, feed_dict={Img: images, Z: z, Z2: z2, K.learning_phase(): 1})
-      gl, samples, sums = outs[:3]
-      writer.add_summary(sums, counter)
-
-      return gl, samples, images
-
-    def f_load():
-      try:
-        return load(sess, saver, checkpoint_dir, name)
-      except:
-        print("Loading weights via Keras")
-        G.load_weights(checkpoint_dir+"/G_weights.keras")
-        D.load_weights(checkpoint_dir+"/D_weights.keras")
-        E.load_weights(checkpoint_dir+"/E_weights.keras")
-
-    def f_save(step):
-      save(sess, saver, checkpoint_dir, step, name)
-      G.save_weights(checkpoint_dir+"/G_weights.keras", True)
-      D.save_weights(checkpoint_dir+"/D_weights.keras", True)
-      E.save_weights(checkpoint_dir+"/E_weights.keras", True)
-
-    def sampler(z, x):
-      code = E.predict(x, batch_size=batch_size)[0]
-      out = G.predict(code, batch_size=batch_size)
-      return out, x
-
-    return train_g, train_d, sampler, f_save, f_load, [G, D, E]
