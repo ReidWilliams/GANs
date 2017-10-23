716befe8f74dae51bebd40fcac22da23a73216b4
Reid Williams <enobarber@gmail.com>
2017-10-23 18:42:00 +0000

    wip

--- discriminator.py
+++ discriminator.py
@@ -78,7 +78,7 @@ class Discriminator():
     self.model = model
     return model
 
-  def diff_loss(self, _x1, _x2):
+  def diff_loss(self, x1, x2):
     ''' Uses similarity layer to return a loss of difference between
     two images. Arguments are image batches. This function operates on
     and returns tensors. '''
@@ -87,8 +87,10 @@ class Discriminator():
     # (None, None, None, None) and was getting exceptions, these lines
     # fixed it. I'm assuming the model just needed explicit tensor shape
 
-    x1 = K.reshape(_x1, (self.batch_size,) + self.img_shape)
-    x2 = K.reshape(_x1, (self.batch_size,) + self.img_shape)
+    #x1 = K.reshape(_x1, (self.batch_size,) + self.img_shape)
+    #x2 = K.reshape(_x1, (self.batch_size,) + self.img_shape)
+
+    print(K.int_shape(x1))
 
     y1 = self.similarity_model(x1)
     y2 = self.similarity_model(x2)
