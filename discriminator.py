87bb28753788b0b9ff36925d8e7d5a95cc53f423
Reid Williams <rwilliams@ideo.com>
2017-10-24 11:13:53 -0700

    wip

--- discriminator.py
+++ discriminator.py
@@ -1,83 +1,50 @@
-"""Keras based implementation of Autoencoding beyond pixels using a learned similarity metric
-
-References:
-
-Autoencoding beyond pixels using a learned similarity metric
-by: Anders Boesen Lindbo Larsen, Soren Kaae Sonderby, Hugo Larochelle, Ole Winther
-https://arxiv.org/abs/1512.09300
-
-Adapted from https://github.com/commaai/research/tree/master/models
-"""
-
-# Keras v.2.0.6
-# https://faroit.github.io/keras-docs/2.0.6/
-
-import os
-os.environ["KERAS_BACKEND"] = "tensorflow"
+import tensorflow as tf
 import numpy as np
-from keras import backend as K
-from keras.layers import (
-  Activation,
-  BatchNormalization as BN,
-  Conv2D,
-  Conv2DTranspose,
-  Cropping2D,
-  Dense, 
-  ELU,
-  Flatten,
-  Input, 
-  Lambda, 
-  Reshape,
-)
-from keras.models import Model
+
+he_init = tf.contrib.layers.variance_scaling_initializer
 
 class Discriminator():
   ''' Discriminator network. Takes in image and outputs predition of whether
   image is real or fake. Has two outputs. One is scalar that is probability
   that an image is fake. Other is a vector that can be used to determine
   similarity between two candidate images. '''
-  def __init__(self, img_shape=(64, 64, 3), batch_size=32):
+  def __init__(self, img_shape=(64, 64, 3), reuse=False):
     # Input image shape: x, y, channels
     self.img_shape = img_shape
-    self.batch_size = batch_size
-    self.build_model()
+    self.reuse = reuse
 
-  def build_model(self):
-    inputs = Input(shape=self.img_shape)
+  def disc(self, inputs):
+    with tf.variable_scope('discriminator', reuse=self.reuse):
     
-    # architecture is similar to autoencoder's encoder. See that for 
-    # detailed comments.
-    t = inputs
-    t = Conv2D(32, 5, strides=2, data_format='channels_last')(t)
-    # according to paper, no BN after first conv layer
-    t = ELU(alpha=1)(t)
+      # architecture is similar to autoencoder's encoder. See that for 
+      # detailed comments.
+      t = tf.layers.conv2d(inputs, 32, 5, strides=2)
+      # according to paper, no BN after first conv layer
+      t = tf.nn.elu(t)
 
-    t = Conv2D(128, 5, strides=2, data_format='channels_last')(t)
-    t = BN(axis=-1)(t)
-    t = ELU(alpha=1)(t)
+      t = tf.layers.conv2d(t, 128, 5, strides=2)
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    t = Conv2D(256, 5, strides=2, data_format='channels_last')(t)
-    t = BN(axis=-1)(t)
-    t = ELU(alpha=1)(t)
+      t = tf.layers.conv2d(t, 256, 5, strides=2)
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    t = Conv2D(256, 5, strides=2, data_format='channels_last')(t)
-    t = BN(axis=-1)(t)
-    t = ELU(alpha=1)(t)
+      t = tf.layers.conv2d(t, 256, 5, strides=2)
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    similarity = Flatten()(t)
+      # use this vector to compare similarity of two images
+      self.similarity = tf.contrib.layers.flatten(t)
 
-    self.similarity_model = Model(inputs=inputs, outputs=similarity)
-     
-    t = Dense(512)(similarity)
-    t = BN(axis=-1)(t)
-    t = ELU(alpha=1)(t)
+      t = tf.layers.dense(self.similarity, 512)
+      t = tf.layers.batch_normalization(t)
+      t = tf.nn.elu(t)
 
-    # output classification: probability an image is fake
-    classification = Dense(1, activation='sigmoid')(t)
-  
-    model = Model(inputs=inputs, outputs=classification)
-    self.model = model
-    return model
+      # output classification: probability an image is fake
+      t = tf.layers.dense(t, 1)
+      classification = tf.sigmoid(t)
+    return classification
 
 
 
