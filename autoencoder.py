59aba79f3bd553f71ce67adfa65ef6576183d46c
Reid Williams <rwilliams@ideo.com>
2017-10-24 10:49:19 -0700

    wip

--- autoencoder.py
+++ autoencoder.py
@@ -49,64 +49,65 @@ class Autoencoder():
       sigmas = tf.exp(0.5 * self.logsigmas) # see Hands on machine learning, Geron, p. 435
 
       sample = tf.random_normal(tf.shape(sigmas), dtype=tf.float32)
-      return sample * sigmas + self.means
+    return sample * sigmas + self.means
 
   def latent_loss(self):
     return 0.5 * tf.reduce_sum(tf.exp(self.logsigmas) + tf.square(self.means) - 1 - self.logsigmas)
 
   def decoder(self, inputs):
-    filters = 64
-    # deconvolution mirrors convolution, start with many filters, then
-    # shrink down to a base level of filters. This is lowest number of filters
-    # before wiring to 3 channel image (rgb).
+    with tf.variable_scope('decoder'):
+      filters = 64
+      # deconvolution mirrors convolution, start with many filters, then
+      # shrink down to a base level of filters. This is lowest number of filters
+      # before wiring to 3 channel image (rgb).
 
-    rows = [int(np.ceil(self.img_shape[0] / i)) for i in [16., 8., 4., 2.]]
-    cols = [int(np.ceil(self.img_shape[1] / i)) for i in [16., 8., 4., 2.]]
-    # What size should image be as we create larger and larger images with
-    # each conv transpose layer.
+      rows = [int(np.ceil(self.img_shape[0] / i)) for i in [16., 8., 4., 2.]]
+      cols = [int(np.ceil(self.img_shape[1] / i)) for i in [16., 8., 4., 2.]]
+      # What size should image be as we create larger and larger images with
+      # each conv transpose layer.
 
-    t = tf.layers.dense(inputs, rows[0]*cols[0]*filters*8, kernel_initializer=he_init())
-    # densely connect z vector to enough units to supply first deconvolution layer.
-    # That's rows*cols and at this layer use 8 times the base number of filters.
+      t = tf.layers.dense(inputs, rows[0]*cols[0]*filters*8, kernel_initializer=he_init())
+      # densely connect z vector to enough units to supply first deconvolution layer.
+      # That's rows*cols and at this layer use 8 times the base number of filters.
 
-    t = tf.reshape(t, (tf.shape(t)[0], rows[0], cols[0], filters*8))
-    # for 64x64 images, this is 4x4 by 512 filters
-    t = tf.layers.batch_normalization(t, axis=-1)
-    t = tf.nn.elu(t)
+      t = tf.reshape(t, (tf.shape(t)[0], rows[0], cols[0], filters*8))
+      # for 64x64 images, this is 4x4 by 512 filters
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    t = tf.layers.conv2d_transpose(t, filters*4, 5, strides=2)
+      t = tf.layers.conv2d_transpose(t, filters*4, 5, strides=2)
 
-    # Because of the way the kernel slides accross
-    # the input, and b/c we're using stride 2, output is double the input
-    # rows/cols plus a few more due to width of kernel. Just crop out extras
-    # before moving on.
-    # e.g. for input image of size 4x4, with 5x5 kernel and stride of 2, 
-    # we get output of 11x11, but want 8x8. Crop off bottom and right of image.
+      # Because of the way the kernel slides accross
+      # the input, and b/c we're using stride 2, output is double the input
+      # rows/cols plus a few more due to width of kernel. Just crop out extras
+      # before moving on.
+      # e.g. for input image of size 4x4, with 5x5 kernel and stride of 2, 
+      # we get output of 11x11, but want 8x8. Crop off bottom and right of image.
 
-    # crop the whole batch
-    t = t[:, :rows[1], :cols[1], :]
-   
-    t = tf.layers.batch_normalization(t, axis=-1)
-    t = tf.nn.elu(t)
+      # crop the whole batch
+      t = t[:, :rows[1], :cols[1], :]
+     
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    t = tf.layers.conv2d_transpose(t, filters*2, 5, strides=2)
-    t = t[:, :rows[2], :cols[2], :]
-    # for 64x64 images, this is 16x16 by 128 filters
-    t = tf.layers.batch_normalization(t, axis=-1)
-    t = tf.nn.elu(t)
+      t = tf.layers.conv2d_transpose(t, filters*2, 5, strides=2)
+      t = t[:, :rows[2], :cols[2], :]
+      # for 64x64 images, this is 16x16 by 128 filters
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    t = tf.layers.conv2d_transpose(t, filters, 5, strides=2)
-    t = t[:, :rows[3], :cols[3], :]
-    # for 64x64 images, this is 32x32 by 64 filters
-    t = tf.layers.batch_normalization(t, axis=-1)
-    t = tf.nn.elu(t)
+      t = tf.layers.conv2d_transpose(t, filters, 5, strides=2)
+      t = t[:, :rows[3], :cols[3], :]
+      # for 64x64 images, this is 32x32 by 64 filters
+      t = tf.layers.batch_normalization(t, axis=-1)
+      t = tf.nn.elu(t)
 
-    t = tf.layers.conv2d_transpose(t, self.img_shape[2], 5, strides=2)
-    self.logits = t[:, :self.img_shape[0], :self.img_shape[1], :]
-    # for 64x64 rgb images, this is 64x64 by 3 channels
+      t = tf.layers.conv2d_transpose(t, self.img_shape[2], 5, strides=2)
+      self.logits = t[:, :self.img_shape[0], :self.img_shape[1], :]
+      # for 64x64 rgb images, this is 64x64 by 3 channels
 
-    t = tf.sigmoid(self.logits)
-   
+      t = tf.sigmoid(self.logits)
+      
     return t
 
 
