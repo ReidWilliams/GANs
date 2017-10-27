b4e14d7e3b4ad68f6d6070cc31e631cb1f143dc8
Reid Williams <rwilliams@ideo.com>
2017-10-26 23:51:11 -0700

    wip

--- discriminator.py
+++ discriminator.py
@@ -29,12 +29,12 @@ class Discriminator():
       t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn2')
       t = tf.nn.elu(t, name='elu3')
 
+      # use this vector to compare similarity of two images
+      self.similarity = tf.contrib.layers.flatten(t)
+
       t = tf.layers.conv2d(t, 256, 5, strides=2, name='conv2d4')
       t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn3')
       t = tf.nn.elu(t, name='elu4')
-
-      # use this vector to compare similarity of two images
-      self.similarity = tf.contrib.layers.flatten(t)
 
       t = tf.layers.dense(self.similarity, 512, name='dense1')
       t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn4')
