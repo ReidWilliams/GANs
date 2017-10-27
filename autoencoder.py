936fde882d8433dd2b45d5f8c813e8f813059565
Reid Williams <rwilliams@ideo.com>
2017-10-26 21:18:24 -0700

    batch variable training as tensor flag

--- autoencoder.py
+++ autoencoder.py
@@ -11,7 +11,7 @@ class Autoencoder():
     # latent (z) vector length
     self.zsize = zsize
 
-  def encoder(self, inputs, scope='encoder', reuse=None):
+  def encoder(self, inputs, training, scope='encoder', reuse=None):
     ''' Returns encoder graph. Inputs is a placeholder of size
     (None, rows, cols, channels) '''
      
@@ -19,32 +19,32 @@ class Autoencoder():
       # Base number of 2D convolution filters. 64 is from paper.
       filters = 64
       # 64 filters of 5x5 field with stride 2
-      t0 = tf.layers.conv2d(inputs, filters, 5, strides=2)
+      t = tf.layers.conv2d(inputs, filters, 5, strides=2)
 
       # Batch normalize per channel (per the paper) and channels are last dim.
       # This means find average accross the batch and apply it to the inputs, 
       # but do it separately for each channel. Also note that in the input layer,
       # we call them channels (red, green, blue) but in deeper layers each channel
       # is the output of a convolution filter.
-      t1 = tf.layers.batch_normalization(t0, axis=-1, training=True)
-      t2 = tf.nn.elu(t1)
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t3 = tf.layers.conv2d(t2, filters*2, 5, strides=2)   
-      t4 = tf.layers.batch_normalization(t3, axis=-1, training=True)
-      t5 = tf.nn.elu(t4)
+      t = tf.layers.conv2d(t, filters*2, 5, strides=2)   
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t6 = tf.layers.conv2d(t5, filters*2, 5, strides=2)   
-      t7 = tf.layers.batch_normalization(t6, axis=-1, training=True)
-      t8 = tf.nn.elu(t7)
+      t = tf.layers.conv2d(t, filters*2, 5, strides=2)   
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t9 = tf.contrib.layers.flatten(t8)
+      t = tf.contrib.layers.flatten(t)
        
       # In a variational autoencoder, the encoder outputs a mean and sigma vector
       # from which samples are drawn. In practice, treat the second output as
       # log(sigma**2), but we'll call it logsigma. Each of mean and logsigma are
       # zsize vectors, but here we pack them into a single zsize*2 vector.
-      self.means = tf.layers.dense(t9, self.zsize, activation=tf.nn.elu, kernel_initializer=he_init())
-      self.logsigmas = tf.layers.dense(t9, self.zsize, activation=tf.nn.elu, kernel_initializer=he_init())
+      self.means = tf.layers.dense(t, self.zsize, activation=tf.nn.elu, kernel_initializer=he_init())
+      self.logsigmas = tf.layers.dense(t, self.zsize, activation=tf.nn.elu, kernel_initializer=he_init())
       # keep means and logsigma for computing variational loss
       sigmas = tf.exp(0.5 * self.logsigmas) # see Hands on machine learning, Geron, p. 435
 
@@ -56,7 +56,7 @@ class Autoencoder():
       loss = 0.5 * tf.reduce_mean(tf.exp(self.logsigmas) + tf.square(self.means) - 1 - self.logsigmas)
     return loss
 
-  def decoder(self, inputs, scope='decoder', reuse=None):
+  def decoder(self, inputs, training, scope='decoder', reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
       filters = 64
       # deconvolution mirrors convolution, start with many filters, then
@@ -68,16 +68,16 @@ class Autoencoder():
       # What size should image be as we create larger and larger images with
       # each conv transpose layer.
 
-      t0 = tf.layers.dense(inputs, rows[0]*cols[0]*filters*8, kernel_initializer=he_init())
+      t = tf.layers.dense(inputs, rows[0]*cols[0]*filters*8, kernel_initializer=he_init())
       # densely connect z vector to enough units to supply first deconvolution layer.
       # That's rows*cols and at this layer use 8 times the base number of filters.
 
-      t1 = tf.reshape(t0, (tf.shape(t0)[0], rows[0], cols[0], filters*8))
+      t = tf.reshape(t, (tf.shape(t0)[0], rows[0], cols[0], filters*8))
       # for 64x64 images, this is 4x4 by 512 filters
-      t2 = tf.layers.batch_normalization(t1, axis=-1, training=True)
-      t3 = tf.nn.elu(t2)
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t4 = tf.layers.conv2d_transpose(t3, filters*4, 5, strides=2)
+      t = tf.layers.conv2d_transpose(t, filters*4, 5, strides=2)
 
       # Because of the way the kernel slides accross
       # the input, and b/c we're using stride 2, output is double the input
@@ -87,30 +87,30 @@ class Autoencoder():
       # we get output of 11x11, but want 8x8. Crop off bottom and right of image.
 
       # crop the whole batch
-      t5 = t4[:, :rows[1], :cols[1], :]
+      t = t[:, :rows[1], :cols[1], :]
      
-      t6 = tf.layers.batch_normalization(t5, axis=-1, training=True)
-      t7 = tf.nn.elu(t6)
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t8 = tf.layers.conv2d_transpose(t7, filters*2, 5, strides=2)
-      t9 = t8[:, :rows[2], :cols[2], :]
+      t = tf.layers.conv2d_transpose(t, filters*2, 5, strides=2)
+      t = t[:, :rows[2], :cols[2], :]
       # for 64x64 images, this is 16x16 by 128 filters
-      t10 = tf.layers.batch_normalization(t9, axis=-1, training=True)
-      t11 = tf.nn.elu(t10)
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t12 = tf.layers.conv2d_transpose(t11, filters, 5, strides=2)
-      t13 = t12[:, :rows[3], :cols[3], :]
+      t = tf.layers.conv2d_transpose(t, filters, 5, strides=2)
+      t = t[:, :rows[3], :cols[3], :]
       # for 64x64 images, this is 32x32 by 64 filters
-      t14 = tf.layers.batch_normalization(t13, axis=-1, training=True)
-      t15 = tf.nn.elu(t14)
+      t = tf.layers.batch_normalization(t, axis=-1, training=training)
+      t = tf.nn.elu(t)
 
-      t16 = tf.layers.conv2d_transpose(t15, self.img_shape[2], 5, strides=2)
-      self.logits = t16[:, :self.img_shape[0], :self.img_shape[1], :]
+      t = tf.layers.conv2d_transpose(t, self.img_shape[2], 5, strides=2)
+      self.logits = t[:, :self.img_shape[0], :self.img_shape[1], :]
       # for 64x64 rgb images, this is 64x64 by 3 channels
 
-      t17 = tf.sigmoid(self.logits)
+      t = tf.sigmoid(self.logits)
 
-    return t17
+    return t
 
 
 
--- discriminator.py
+++ discriminator.py
@@ -12,7 +12,7 @@ class Discriminator():
     # Input image shape: x, y, channels
     self.img_shape = img_shape
 
-  def disc(self, inputs, scope='discriminator', reuse=None):
+  def disc(self, inputs, training, scope='discriminator', reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
     
       # architecture is similar to autoencoder's encoder. See that for 
@@ -22,22 +22,22 @@ class Discriminator():
       t = tf.nn.elu(t, name='elu1')
 
       t = tf.layers.conv2d(t, 128, 5, strides=2, name='conv2d2')
-      t = tf.layers.batch_normalization(t, axis=-1, training=True, name='bn1')
+      t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn1')
       t = tf.nn.elu(t, name='elu2')
 
       t = tf.layers.conv2d(t, 256, 5, strides=2, name='conv2d3')
-      t = tf.layers.batch_normalization(t, axis=-1, training=True, name='bn2')
+      t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn2')
       t = tf.nn.elu(t, name='elu3')
 
       t = tf.layers.conv2d(t, 256, 5, strides=2, name='conv2d4')
-      t = tf.layers.batch_normalization(t, axis=-1, training=True, name='bn3')
+      t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn3')
       t = tf.nn.elu(t, name='elu4')
 
       # use this vector to compare similarity of two images
       self.similarity = tf.contrib.layers.flatten(t)
 
       t = tf.layers.dense(self.similarity, 512, name='dense1')
-      t = tf.layers.batch_normalization(t, axis=-1, training=True, name='bn4')
+      t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn4')
       t = tf.nn.elu(t, name='elu5')
 
       # output classification: probability an image is fake
