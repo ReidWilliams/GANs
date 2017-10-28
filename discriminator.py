f8891b3851fb68f3b1426732093268e05bdf6c8d
Reid Williams <rwilliams@ideo.com>
2017-10-28 16:39:32 -0700

    similarity to after 2nd conv layer

--- discriminator.py
+++ discriminator.py
@@ -21,12 +21,12 @@ class Discriminator():
       # according to paper, no BN after first conv layer
       t = tf.nn.elu(t, name='elu1')
 
-      # use this vector to compare similarity of two images
-      self.similarity = tf.contrib.layers.flatten(t)
-
       t = tf.layers.conv2d(t, 128, 5, strides=2, name='conv2d2')
       t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn1')
       t = tf.nn.elu(t, name='elu2')
+
+      # use this vector to compare similarity of two images
+      self.similarity = tf.contrib.layers.flatten(t)
 
       t = tf.layers.conv2d(t, 256, 5, strides=2, name='conv2d3')
       t = tf.layers.batch_normalization(t, axis=-1, training=training, name='bn2')
