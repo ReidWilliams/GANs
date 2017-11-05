3a55db992f8a4eec0e4c270f59d344c1a1f4b9e9
Reid Williams <rwilliams@ideo.com>
2017-11-05 10:19:49 -0800

    bug fix

--- utils.py
+++ utils.py
@@ -62,9 +62,6 @@ def tile(images, size):
     if (images.shape[3] in (3,4)):
         c = images.shape[3]
         img = np.zeros((h * size[0], w * size[1], c))
-        print('images shape')
-        print(images.shape)
-        print('tile size %s' % img.shape)
         for idx, image in enumerate(images):
             i = idx % size[1]
             j = idx // size[1]
