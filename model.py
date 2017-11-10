975db288f003ba2c69f2abebe1ee13d9b5903f55
Reid Williams <rwilliams@ideo.com>
2017-11-09 20:53:59 -0800

    bug fix

--- model.py
+++ model.py
@@ -235,9 +235,9 @@ class Model:
         path = os.path.join(self.dirs['output'], '%06d.jpg' % self.output_img_idx)
         tiled = pixels01(tile(imgs, (rows, cols)))
         
-        for r in range(0, rows, 2):
+        for r in range(0, rows/2):
             for c in range(0, cols):
-                tiled[r*cols + col] = feed[r*cols + col]
+                tiled[r*2*cols + c] = feed[r*cols + c]
 
         as_ints = (tiled * 255.0).astype('uint8')
         Image.fromarray(as_ints).save(path)
