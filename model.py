2b9ed89707120f4b6ef469f293503adcd70b841c
Reid Williams <rwilliams@ideo.com>
2017-11-13 15:51:52 -0800

    WGAN with gradient penalty loss instead of weight clipping

--- model.py
+++ model.py
@@ -76,8 +76,20 @@ class Model:
             self.arch.discriminator(self.Gz, reuse=True)
 
     def build_losses(self):
-        # WGAN loss
-        self.D_loss = tf.reduce_mean(self.Dreal_logits - self.Dz_logits)
+        # Wasserstein GAN with gradient penalty
+        # from github.com/lilianweng/unified-gan-tensorflow
+        epsilon = tf.random_uniform([self.batch_size, 1, 1, 1], 0.0, 1.0)
+        interpolated = epsilon * self.X + (1 - epsilon) * self.Gz
+
+        _, Dinterpolated_logits, _ = \
+            self.arch.discriminator(interpolated, reuse=True)
+
+        # tf.gradients returns a list of sum(dy/dx) for each x in xs.
+        gradients = tf.gradients(Dinterpolated_logits, [interpolated, ])[0]
+        grad_l2 = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]))
+        grad_penalty = tf.reduce_mean(tf.square(grad_l2 - 1.0))
+
+        self.D_loss = tf.reduce_mean(self.Dreal_logits - self.Dz_logits) + grad_penalty
         self.G_loss = tf.reduce_mean(self.Dz_logits)
 
     def build_optimizers(self):
