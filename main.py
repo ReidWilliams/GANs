c7b1b326a297ef8fd75425055b704712ff728175
Reid Williams <rwilliams@ideo.com>
2017-11-15 18:31:25 -0800

    batch size is command line option

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from model import Model
 parser = argparse.ArgumentParser()
 parser.add_argument('--datadir', required=True, dest='datadir')
 parser.add_argument('--savefreq', default=10, dest='save_freq')
+parser.add_argument('--batchsize', default=64, dest='batch_size')
 parsed = parser.parse_args()
 
-model = Model(parsed.datadir, save_freq=parsed.save_freq, img_shape=(256, 448))
+model = Model(parsed.datadir, save_freq=parsed.save_freq, 
+    batch_size=parsed.batch_size, img_shape=(256, 448))
 model.build_model()
 model.build_losses()
 model.build_optimizers()
